1f791998c671a7712558fd5cb6389ee9f5b2f2d3
hkwak60 <hkwak60@gmail.com>
2024-09-25 11:50:41 -0400

    GUI Leaflet Implementation
    Allocated a slot for leaflet map tiler, can update maps with districts here. Planning to upload the map of the States on the first page

--- TrueVoiceUI/src/App.jsx
+++ TrueVoiceUI/src/App.jsx
@@ -1,8 +1,21 @@
 import { useState } from "react";
 import "./App.css";
 
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 function App() {
-  return <h1 className="title">True Voice</h1>;
+  return (
+    <>
+      <h1 className="title">True Voice</h1>
+      <MapContainer center={[48.8566, 2.3522]} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </>
+  );
 }
 
 export default App;
