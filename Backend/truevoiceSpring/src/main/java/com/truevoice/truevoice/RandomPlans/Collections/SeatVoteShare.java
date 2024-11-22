cc9abe232df8b27ba7ea59217660c7de82b6ff21
hkwak60 <hkwak60@gmail.com>
2024-11-22 00:18:51 -0500

    Server Implementation

---
+++ Backend/truevoiceSpring/src/main/java/com/truevoice/truevoice/RandomPlans/Collections/SeatVoteShare.java
@@ -0,0 +1,22 @@
+package com.truevoice.truevoice.RandomPlans.Collections;
+
+import java.util.List;
+
+import lombok.Data;
+@Data
+public class SeatVoteShare {
+    private double voteShare;
+    private double smdSeatShare;
+    private double mmdSeatShare;
+    private double symmetry;
+    private double bias;
+    private double responsiveness;
+    private List<ElectionResult> electionResults;
+
+    @Data
+    public static class ElectionResult {
+        private int districtId;
+        private double seatShare;
+        private double voteShare;
+    }
+}
