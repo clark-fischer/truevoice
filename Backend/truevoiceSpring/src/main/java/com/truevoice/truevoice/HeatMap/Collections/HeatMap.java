2c455880a5051e6754d5f2082a5fcdf6a2f3e0e1
hkwak60 <hkwak60@gmail.com>
2024-12-01 23:13:13 -0500

    Heatmap Demographics

---
+++ Backend/truevoiceSpring/src/main/java/com/truevoice/truevoice/HeatMap/Collections/HeatMap.java
@@ -0,0 +1,20 @@
+package com.truevoice.truevoice.HeatMap.Collections;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.truevoice.truevoice.FRAEnum.FIPS;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "heatmaps")
+public class HeatMap {
+    private FIPS fips;
+    private List<Demographics> demographics;
+}
