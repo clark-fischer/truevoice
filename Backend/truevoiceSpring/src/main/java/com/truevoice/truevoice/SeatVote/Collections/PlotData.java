60529df52ac6f648939492e88edf1410e08a41c4
hkwak60 <hkwak60@gmail.com>
2024-12-07 16:22:54 -0500

    seatvote updated

--- Backend/truevoiceSpring/src/main/java/com/truevoice/truevoice/SeatVote/Collections/PlotData.java
+++ Backend/truevoiceSpring/src/main/java/com/truevoice/truevoice/SeatVote/Collections/PlotData.java
@@ -4,7 +4,8 @@ import lombok.Data;
 
 @Data
 public class PlotData {
-    private int district_id;
-    private double vote_share;
-    private double seat_share;
+    private int districtId;
+    private double demVoteShare;
+    private double repVoteShare;
+    private int totalVotes;
 }
